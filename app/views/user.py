b4510d3d17bfe1ad83dc3cae0993498ff7ed8b80
riya <riyagupta@tristonsoft.com>
2019-09-28 14:28:25 +0530

    new app email configuration changes

--- app/views/user.py
+++ app/views/user.py
@@ -61,12 +61,9 @@ def signup():
         # Build a confirm link with token
         confirmUrl = url_for('userbp.confirm', token=token, _external=True)
         # Render an HTML template to send by email
-        # html = render_template('email/confirm.html',
-        #                        confirm_url=confirmUrl)
+        html = render_template('email/confirm.html',
+                               confirm_url=confirmUrl)
         
-        html = "please confirm your email"
-        html += confirmUrl 
-        html += "Thanks"
         # Send the email to user
         try:
             email.send(user.email, subject, html)
